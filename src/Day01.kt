d8aa30ce05b3b2e114aed409390cb4cda0098622
wgolyakov <wolodia@narod.ru>
2024-07-02 18:32:11 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>) = input.sumOf { it.toInt() }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part2(input: List<String>): Int {
+		val changes = input.map { it.toInt() }
+		var frequency = 0
+		val values = mutableSetOf(frequency)
+		while (true) {
+			for (change in changes) {
+				frequency += change
+				if (frequency in values) return frequency
+				values.add(frequency)
+			}
+		}
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun changes(input: List<String>, num: Int) = input[num].split(", ")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+	val testInput = readInput("Day01_test")
+	check(part1(changes(testInput, 0)) == 3)
+	check(part1(changes(testInput, 1)) == 3)
+	check(part1(changes(testInput, 2)) == 0)
+	check(part1(changes(testInput, 3)) == -6)
+
+	check(part2(changes(testInput, 0)) == 2)
+	check(part2(changes(testInput, 4)) == 0)
+	check(part2(changes(testInput, 5)) == 10)
+	check(part2(changes(testInput, 6)) == 5)
+	check(part2(changes(testInput, 7)) == 14)
+
+	val input = readInput("Day01")
+	part1(input).println()
+	part2(input).println()
 }
